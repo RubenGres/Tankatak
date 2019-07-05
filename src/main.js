7c7aa7cf9023cc6720bd5c0e0c145e023b2242a9
Retronyme <rubengres@hotmail.fr>
2019-07-06 00:37:44 +0200

    changed asset path to absolute

--- src/main.js
+++ src/main.js
@@ -34,19 +34,19 @@ let game = new Phaser.Game(config);
 /* reference the functions in files */
 function preload(){
     game = this;
-    this.load.image('tank_gun', '../assets/tank_gun.png');
-    this.load.spritesheet('tank', '../assets/tank.png', { frameWidth: 93, frameHeight: 48 });
+    this.load.image('tank_gun', 'assets/tank_gun.png');
+    this.load.spritesheet('tank', 'assets/tank.png', { frameWidth: 93, frameHeight: 48 });
     
     /*backgrounds*/
     bkg_jungle = new background();
-    bkg_jungle.preload('jungle', '../assets/bkg/jungle', 6);
+    bkg_jungle.preload('jungle', 'assets/bkg/jungle', 6);
 }
 
 function create(){
     bkg_jungle.create();
-
+    
     addPlatform(0,GROUNDLEVEL,config.width,1);
-
+    
     /* add player */
     player = new player(100, GROUNDLEVEL - 30, this);
 
